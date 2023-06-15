cdbcf262b423035e1882a633709b31e8dc721286
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2023-06-16 00:02:23 +0100

    fixing it for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function HomePage() {
   return (
     <div>
       <section>
-        <div className="md:hidden flex items-start my-8 flex-col md:flex-row">
+        <div className="md:hidden flex items-start my-8 flex-row">
           <Image
             alt={name}
             className="rounded-full"
@@ -25,7 +25,7 @@ export default async function HomePage() {
             width={200}
             priority
           />
-          <div className="mt-2 ml-0 space-y-2 flex flex-col dark:text-neutral-500">
+          <div className="ml-2 space-y-2 flex flex-col dark:text-neutral-500">
             <a
               rel="noopener noreferrer"
               target="_blank"
@@ -51,54 +51,60 @@ export default async function HomePage() {
               <LinkedInIcon />
             </a>
           </div>
-          <p className="my-5 text-neutral-800 dark:text-neutral-600">
-            {about()}
-          </p>
-          <p className="my-5 text-neutral-800 dark:text-neutral-600">
-            {bio()}
-          </p>
+          <div className="mt-5">
+            <p className="text-neutral-800 dark:text-neutral-600">
+              {bio()}
+            </p>
+            <p className="mt-5 text-neutral-800 dark:text-neutral-600">
+              {about()}
+            </p>
+          </div>
         </div>
-        <div className="hidden md:flex items-start md:items-center my-8 flex-col">
-          <Image
-            alt={name}
-            className="rounded-full"
-            src={avatar}
-            placeholder="blur"
-            width={200}
-            priority
-          />
-          <div className="mt-8 ml-0 md:ml-6 space-y-2 flex flex-col dark:text-neutral-500">
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://twitter.com/roboticparts"
-              className="items-center gap-2 dark:hover:text-neutral-900"
-            >
-              <TwitterIcon />
-            </a>
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://github.com/effieklimi"
-              className="items-center gap-2 dark:hover:text-neutral-900"
-            >
-              <GitHubIcon />
-            </a>
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://linkedin.com/effieklimi"
-              className="items-center gap-2 dark:hover:text-neutral-900"
-            >
-              <LinkedInIcon />
-            </a>
+        <div className="hidden md:flex items-start my-8 flex-col justify-end">
+          <div className="flex flex-row items-center space-x-8">
+            <Image
+              alt={name}
+              className="rounded-full"
+              src={avatar}
+              placeholder="blur"
+              width={200}
+              priority
+            />
+            <div className="space-y-2 flex flex-col dark:text-neutral-500">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://twitter.com/roboticparts"
+                className="items-center gap-2 dark:hover:text-neutral-900"
+              >
+                <TwitterIcon />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/effieklimi"
+                className="items-center gap-2 dark:hover:text-neutral-900"
+              >
+                <GitHubIcon />
+              </a>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://linkedin.com/effieklimi"
+                className="items-center gap-2 dark:hover:text-neutral-900"
+              >
+                <LinkedInIcon />
+              </a>
+            </div>
           </div>
-          <p className="my-5 text-neutral-800 dark:text-neutral-600">
-            {about()}
-          </p>
-          <p className="my-5 text-neutral-800 dark:text-neutral-600">
-            {bio()}
-          </p>
+          <div className="mt-5 text-left">
+            <p className="text-neutral-800 dark:text-neutral-600">
+              {bio()}
+            </p>
+            <p className="mt-5 text-neutral-800 dark:text-neutral-600">
+              {about()}
+            </p>
+          </div>
         </div>
       </section>
     </div>
