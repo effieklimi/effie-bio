7e8f40a44a816b8fc8321662ff6545f3db9d2cc4
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2023-06-15 23:35:32 +0100

    fixing it for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -15,71 +15,92 @@ export default async function HomePage() {
 
   return (
     <div>
-    <section>
-      {/* <h1 className="font-bold text-3xl font-serif">{name}</h1> */}
-      <div className="flex items-start md:items-center my-8 flex-col md:flex-row">
-        <Image
-          alt={name}
-          className="rounded-full"
-          src={avatar}
-          placeholder="blur"
-          width={200}
-          priority
-        />
-        <div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 dark:text-neutral-500">
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://twitter.com/roboticparts"
-            className="flex items-center gap-2 dark:hover:text-neutral-900"
-          >
-            <TwitterIcon />
-          </a>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/effieklimi"
-            className="flex items-center gap-2 dark:hover:text-neutral-900"
-          >
-            <GitHubIcon />
-          </a>
-          <a
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://linkedin.com/effieklimi"
-            className="flex items-center gap-2 dark:hover:text-neutral-900"
-          >
-            <LinkedInIcon />
-          </a>
+      <section>
+        <div className="md:hidden flex items-start my-8 flex-col">
+          <Image
+            alt={name}
+            className="rounded-full"
+            src={avatar}
+            placeholder="blur"
+            width={200}
+            priority
+          />
+          <div className="mt-2 ml-0 space-y-2 flex flex-row dark:text-neutral-500">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://twitter.com/roboticparts"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/effieklimi"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://linkedin.com/effieklimi"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <LinkedInIcon />
+            </a>
+          </div>
+          <p className="my-5 text-neutral-800 dark:text-neutral-600">
+            {about()}
+          </p>
+          <p className="my-5 text-neutral-800 dark:text-neutral-600">
+            {bio()}
+          </p>
         </div>
-      </div>
-      <p className="my-5 text-neutral-800 dark:text-neutral-600">
-        {about()}
-      </p>
-      <p className="my-5 text-neutral-800 dark:text-neutral-600">
-        {bio()}
-      </p>
-      <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-500 dark:text-neutral-400">
-        <li>
-          <a
-            className="flex items-center hover:text-neutral-900 dark:hover:text-neutral-400 dark:text-neutral-900 transition-all"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="mailto:effie@effie.bio"
-          >
-            <ArrowIcon />
-            <p className="h-7 font-bold">Email me</p>
-          </a>
-        </li>
-      </ul>
-    </section>
-
-    {/* <div className='wrapper'>
-      <div className="push"></div>
-    </div>
-    <div className="footer">
-      <FooterCentered />
-    </div> */}
+        <div className="hidden md:flex items-start md:items-center my-8 flex-col md:flex-row">
+          <Image
+            alt={name}
+            className="rounded-full"
+            src={avatar}
+            placeholder="blur"
+            width={200}
+            priority
+          />
+          <div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 flex flex-col dark:text-neutral-500">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://twitter.com/roboticparts"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/effieklimi"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://linkedin.com/effieklimi"
+              className="items-center gap-2 dark:hover:text-neutral-900"
+            >
+              <LinkedInIcon />
+            </a>
+          </div>
+          <p className="my-5 text-neutral-800 dark:text-neutral-600">
+            {about()}
+          </p>
+          <p className="my-5 text-neutral-800 dark:text-neutral-600">
+            {bio()}
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
   }
 
   return (
-    <aside className="md:w-[200px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif">
+    <aside className="md:w-[150px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif hidden md:flex">
       <div className="lg:sticky lg:top-20">
         <div className="ml-2 md:ml-[12px] mb-2 px-4 md:px-0 md:mb-8 space-y-10 flex flex-col items-start ">
           <Logo />
@@ -85,7 +85,7 @@ export default function Navbar() {
             className="flex flex-col items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
             id="nav"
           >
-            <div className="flex flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0 text-lg">
+            <div className="flex flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
               {Object.entries(navItems).map(([path, { name }]) => {
                 const isActive = path === pathname;
                 return (
