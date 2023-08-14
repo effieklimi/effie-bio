5e8ab9dd65d06b50be7388bb85f8129109c476ff
Effie Klimi <42475826+effieklimi@users.noreply.github.com>
2023-08-14 13:20:30 +0100

    changed photo

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,44 +1,44 @@
-import me from '../app/avatar.jpg';
-import ek from '../app/ek.jpg';
+import me from "../app/avatar-madrid.jpg";
+import ek from "../app/ek.jpg";
 
-export const name = 'Effie Klimi';
+export const name = "Effie Klimi";
 export const avatar = me;
 export const logo = ek;
 export const about = () => {
   return (
     <>
-      I'm <span className="highlight-container"><span className="highlight"><b>Effie.</b></span></span> I do research in biology and build software to accelerate science.
+      I'm{" "}
+      <span className="highlight-container">
+        <span className="highlight">
+          <b>Effie.</b>
+        </span>
+      </span>{" "}
+      I do research in biology and build software to accelerate science.
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      I've worked on basic research, evolutionary genetics, structural omics and my PhD 
-      in RNA therapeutics & viral vectors @ The University of Edinburgh.
+      I've worked on basic research, evolutionary genetics, structural omics and
+      my PhD in RNA therapeutics & viral vectors @ The University of Edinburgh.
     </>
-    
   );
 };
 
 export const coding = () => {
   return (
     <>
-      I care about the intersection of science and software that works and looks great. 
-      If you want to discuss (your) biotech startup, data and software,&nbsp; 
+      I care about the intersection of science and software that works and looks
+      great. If you want to discuss (your) biotech startup, data and
+      software,&nbsp;
       <a href="mailto:effie@effie.bio">
         <b>
           <span className="highlight-container">
-            <span className="highlight">
-                email me!&nbsp;
-            </span>
+            <span className="highlight">email me!&nbsp;</span>
           </span>
         </b>
-
       </a>
-
-      
     </>
-    
   );
 };
